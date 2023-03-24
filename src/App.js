86154e2bfd6538f14994f3b3191485fdaaea09f0
nyan92213 <ivanbezrukov92213@gmail.com>
2023-03-24 10:41:56 +0300

    added useCallback, useMemo, useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import {Component, useState, useEffect} from 'react';
+import {Component, useState, useEffect, useCallback, useMemo} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
+
+const countTotal = (num) => {
+    console.log('counting...')
+    return num + 10
+}
+
 // class Slider extends Component {
 
 //     constructor(props) {
@@ -60,14 +66,46 @@ import './App.css';
 //     return Math.random() * (50 - 1) + 1
 // }
 
+const getSomeImages = () => {
+    console.log('fetching')
+    return [
+        "https://klike.net/uploads/posts/2019-05/1556708032_1.jpg",
+        "https://bipbap.ru/wp-content/uploads/2021/08/1547365435_25.jpg"
+    ]
+}
+
 const Slider = (props) => {
 
     const [slide, setSlide] = useState(0)
     const [autoplay, setAutoplay] = useState(false)
+
+    const getSomeImages = useCallback(    () => {
+        console.log('fetching')
+        return [
+            "https://klike.net/uploads/posts/2019-05/1556708032_1.jpg",
+            "https://bipbap.ru/wp-content/uploads/2021/08/1547365435_25.jpg"
+        ]
+    }, [slide])
     
-    useEffect(() => {
-        document.title = `Slide ${slide}`
-    })
+    function logging() {
+        console.log('log!')
+    }
+    
+    // useEffect(() => {
+    //     console.log('effect')
+    //     document.title = `Slide ${slide}`
+
+    //     // window.addEventListener('click', logging)
+
+    //     // return () => {
+    //     //     window.removeEventListener('click', logging)
+    //     // }
+
+    // }, [slide]) // можно отсавить пустым, для эмулировани componentDidMount когда функция выполняется только при создании страницы
+
+    // useEffect(() => {
+    //     console.log('autoplay')
+    // }, [autoplay])
 
     // const [state, setState] = useState({slide: 0, autoplay: false})
 
@@ -87,12 +125,25 @@ const Slider = (props) => {
         setAutoplay(autoplay => !autoplay)
     }
 
+    const total = useMemo(() => {
+        return countTotal(slide)
+    }, [slide])
+
+    const style = useMemo(() => ({
+        color: slide > 4 ? 'red' : 'black'
+    }), [slide])
+
+    useEffect(() => {
+        console.log('styles')
+    }, [style])
 
     return (
         <Container>
             <div className="slider w-50 m-auto">
-                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <Slide getSomeImages={getSomeImages}/>
+
                 <div className="text-center mt-5">Active slide {slide} <br /> {autoplay ? 'auto' : null}</div>
+                <div style={style} className="text-center mt-5">TOtal slides {total} <br /> {autoplay ? 'auto' : null}</div>
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
@@ -109,13 +160,31 @@ const Slider = (props) => {
     )
 }
 
+const Slide = ({getSomeImages}) => {
+    const [images, setImages] = useState([])
+
+    useEffect(() => {
+        setImages(getSomeImages())
+    }, [getSomeImages])
+
+    return (
+        <>
+            {images.map((url, i) => <img key={i}className="d-block w-100" src={url} alt="slide" />)}
+        </>
+    )
+}
+
 
 function App() {
-  return (
-    <>
-        <Slider/>
-    </>
-  );
+
+    const [slider, setSlider] = useState(true)
+
+    return (
+        <>
+            <button onClick={() => setSlider(false)}>Click</button>
+            {slider ? <Slider/> : null}
+        </>
+    );
 }
 
 export default App;
