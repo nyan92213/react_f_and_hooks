2c2c7972e2bdead232f91fd3f84568b395ad23a4
nyan92213 <ivanbezrukov92213@gmail.com>
2023-04-09 11:16:17 +0300

    added HOC

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import App_016 from "./016 App";
 import App_018 from "./018 App";
+import App_019 from "./019 App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -11,5 +12,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <App/>
   // <App_016 />
-  <App_018 />
+  // <App_018 />
+  <App_019 />
 );
