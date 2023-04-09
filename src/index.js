17eb0912563f0f506b3b480dab1896e870464f75
nyan92213 <ivanbezrukov92213@gmail.com>
2023-04-09 10:24:20 +0300

    useReducer

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import App_016 from "./016 App";
+import App_018 from "./018 App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <App/>
-  <App_016 />
+  // <App_016 />
+  <App_018 />
 );
